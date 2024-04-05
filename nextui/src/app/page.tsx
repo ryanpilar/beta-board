a2e5ac1572d02d2f6509ef0b9a6dbff369326984
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2024-04-04 23:00:03 -0600

    Set up Next and Tailwind project, scafold MaxWidthWarapper.

---
+++ nextui/src/app/page.tsx
@@ -0,0 +1,10 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className="bg-blue-400">
+      <h1>THESE  are children props</h1>
+    </MaxWidthWrapper> 
+  );
+}
