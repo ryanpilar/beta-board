46e5da194022cdad906bc3503b6d453140bcc0f6
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2024-05-25 16:37:27 -0600

    May 25th clean house

--- nextui/src/app/setup/page.tsx
+++ nextui/src/app/setup/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import Link from "next/link"
 import { CircleUser, Menu, Package2, Search } from "lucide-react"
 
--- nextui/src/components/DND.tsx
+++ nextui/src/components/DND.tsx
@@ -1,24 +1,24 @@
 "use client"
 
 import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
+    DndContext,
+    closestCenter,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
 } from '@dnd-kit/core';
 
 import {
-  arrayMove,
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates,
+    verticalListSortingStrategy,
 } from '@dnd-kit/sortable';
 
 import {
-  restrictToVerticalAxis,
-  restrictToParentElement,
+    restrictToVerticalAxis,
+    restrictToParentElement,
 } from '@dnd-kit/modifiers';
 import { useState } from 'react';
 import SortableLinks from '@/components/SortableLinks';
@@ -26,85 +26,85 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { AddNewItem } from '@/components/AddNewItem';
 
 interface Item {
-  name: string;
-  id: number;
+    name: string;
+    id: number;
 }
 interface ItemProps {
-  name: string
-  id: number
+    name: string
+    id: number
 }
 interface DNDProps {
-  defaultItems: Item[]
-  label: string
-  setMainWindow: any
+    defaultItems: Item[]
+    label: string
+    setMainWindow: any
 }
 
 const DND: React.FC<DNDProps> = ({ defaultItems, label, setMainWindow }) => {
 
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
 
-  const [items, setItems] = useState<Item[]>(defaultItems);
+    const [items, setItems] = useState<Item[]>(defaultItems);
 
-  function handleDragEnd(event: any) {
-    const { active, over } = event;
+    function handleDragEnd(event: any) {
+        const { active, over } = event;
 
-    if (active.id !== over.id) {
-      setItems((prevItems) => {
-        const oldIndex = prevItems.findIndex((item) => item.id === active.id);
-        const newIndex = prevItems.findIndex((item) => item.id === over.id);
+        if (active.id !== over.id) {
+            setItems((prevItems) => {
+                const oldIndex = prevItems.findIndex((item) => item.id === active.id);
+                const newIndex = prevItems.findIndex((item) => item.id === over.id);
 
-        return arrayMove(prevItems, oldIndex, newIndex);
-      });
+                return arrayMove(prevItems, oldIndex, newIndex);
+            });
+        }
     }
-  }
 
-  function handleDelete(idToDelete: number) {
-    setItems((prevItems) => prevItems.filter((item) => item.id !== idToDelete));
-  }
+    function handleDelete(idToDelete: number) {
+        setItems((prevItems) => prevItems.filter((item) => item.id !== idToDelete));
+    }
 
-  let idx = Date.now();
+    let idx = Date.now();
 
-  function addNewItem(newItem: string) {
-    setItems((prevItems) => [...prevItems, { name: newItem, id: idx }]);
-  }
+    function addNewItem(newItem: string) {
+        setItems((prevItems) => [...prevItems, { name: newItem, id: idx }]);
+    }
 
-  return (
-    <main className='flex justify-center px-2 mx-auto select-none'>
-      <Card className='w-full md:max-w-lg'>
-        <CardHeader className='space-y-1 '>
-          <CardTitle className='text-2xl flex justify-between'>
-            {label}
-            <AddNewItem addNewItem={addNewItem} />
-          </CardTitle>
-          <CardDescription>List Popular web development frameworks</CardDescription>
-        </CardHeader>
-        <CardContent className='grid gap-4'>
-          <DndContext
-            sensors={sensors}
-            collisionDetection={closestCenter}
-            onDragEnd={handleDragEnd}
-            modifiers={[restrictToVerticalAxis, restrictToParentElement]}
-          >
-            <SortableContext items={items} strategy={verticalListSortingStrategy}>
-              {items.map((item, index) => (
+    return (
+        <main className='flex justify-center px-2 mx-auto select-none'>
+            <Card className='w-full md:max-w-lg'>
+                <CardHeader className='space-y-1 '>
+                    <CardTitle className='text-2xl flex justify-between'>
+                        {label}
+                        <AddNewItem addNewItem={addNewItem} />
+                    </CardTitle>
+                    <CardDescription>List Popular web development frameworks</CardDescription>
+                </CardHeader>
+                <CardContent className='grid gap-4'>
+                    <DndContext
+                        sensors={sensors}
+                        collisionDetection={closestCenter}
+                        onDragEnd={handleDragEnd}
+                        modifiers={[restrictToVerticalAxis, restrictToParentElement]}
+                    >
+                        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+                            {items.map((item, index) => (
 
-                // <div className='cursor-pointer' onClick={() => {
-                //   setMainWindow(index)
-                // }}>
-                  <SortableLinks key={item.id} id={item} onDelete={handleDelete} changeView={()=>setMainWindow(index+1)} />
-                // </div>
-              ))}
-            </SortableContext>
-          </DndContext>
-        </CardContent>
-      </Card>
-    </main>
-  );
+                                // <div className='cursor-pointer' onClick={() => {
+                                //   setMainWindow(index)
+                                // }}>
+                                <SortableLinks key={item.id} id={item} onDelete={handleDelete} changeView={() => setMainWindow(index + 1)} />
+                                // </div>
+                            ))}
+                        </SortableContext>
+                    </DndContext>
+                </CardContent>
+            </Card>
+        </main>
+    );
 };
 
 export default DND;
